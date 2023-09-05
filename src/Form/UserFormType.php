767c0ce68f6ae0f48d1373af7eb70bbcdde89c90
LicorneBleu <guillaume.couget@gmail.com>
2023-09-05 22:33:30 +0200

    update userForm

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,11 @@ class UserFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class)
             ->add('roles')
             ->add('password')
-            ->add('createdAt')
+            ->add('createdAt', DateType::class)
+            ->add('save', SubmitType::class)
         ;
     }
 
